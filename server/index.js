3567bce1edcaceef58809365595a8ceaee7b41d2
ansh35 <khareansh075@gmail.com>
2026-06-02 12:37:49 +0530

    debug: add request logging to backend

--- server/index.js
+++ server/index.js
@@ -17,6 +17,12 @@ connectDB();
 
 const app = express();
 
+// Request Logging
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 // Swagger Documentation
 setupSwagger(app);
 
